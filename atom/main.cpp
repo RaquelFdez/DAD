3d2316bc7f2c85569a9b64502a06d649b4d36d9a
RaquelFdez <47743846+RaquelFdez@users.noreply.github.com>
2019-06-15 18:10:23 +0200

    Create main.cpp

---
+++ atom/main.cpp
@@ -0,0 +1,320 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Servo.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "MiFibra-E984";
+const char* password = "3WYQNXiL";
+const char* channel_name = "topic_2";
+const char* mqtt_server = "192.168.1.62";
+const char* http_server = "192.168.1.62";
+const char* http_server_port = "8090";
+String clientId;
+int peso=0;
+int hora=0;
+int pesoLeido=0;
+Servo servo;
+const int fsrPin = A0;
+int fsrReading = 0;
+
+const long utcOffsetInSeconds = 3600*2;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+long lastMsgRest = 0;
+char msg[50];
+int value = 0;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "hora.rediris.es", utcOffsetInSeconds);
+// Conexión a la red WiFi
+void setup_wifi() {
+
+  delay(10);
+
+  // Fijamos la semilla para la generación de número aleatorios. Nos hará falta
+  // más adelante para generar ids de clientes aleatorios
+  randomSeed(micros());
+
+  Serial.println();
+  Serial.print("Conectando a la red WiFi ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  // Mientras que no estemos conectados a la red, seguimos leyendo el estado
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // En este punto el ESP se encontrará registro en la red WiFi indicada, por
+  // lo que es posible obtener su dirección IP
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.println("Dirección IP registrada: ");
+
+
+
+  Serial.println(WiFi.localIP());
+
+}
+
+// Método llamado por el cliente MQTT cuando se recibe un mensaje en un canal
+// al que se encuentra suscrito. Los parámetros indican el canal (topic),
+// el contenido del mensaje (payload) y su tamaño en bytes (length)
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje recibido [canal: ");
+  Serial.print(topic);
+  Serial.print("] ");
+  // Leemos la información del cuerpo del mensaje. Para ello no sólo necesitamos
+  // el puntero al mensaje, si no su tamaño.
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+
+  DynamicJsonDocument doc(length);
+  deserializeJson(doc, payload, length);
+  const char* action = doc["action"];
+  Serial.printf("Acción %s\n", action);
+  // Encendemos un posible switch digital (un diodo led por ejemplo) si el
+  // contenido del cuerpo es 'on'
+  if (strcmp(action, "on") == 0) {
+    digitalWrite(BUILTIN_LED, HIGH);
+    Serial.println("Detectada acción de activación");
+  } else if (strcmp(action, "off") == 0) {
+    digitalWrite(BUILTIN_LED, LOW);
+    Serial.println("Detectada acción de desactivación");
+  } else{
+    Serial.println("Acción no reconocida");
+  }
+}
+
+// Función para la reconexión con el servidor MQTT y la suscripción al canal
+// necesario. También se fija el identificador del cliente
+void reconnect() {
+  // Esperamos a que el cliente se conecte al servidor
+  while (!client.connected()) {
+    Serial.print("Conectando al servidor MQTT...");
+    // Creamos un identificador de cliente aleatorio. Cuidado, esto debe
+    // estar previamente definido en un entorno real, ya que debemos
+    // identificar al cliente de manera unívoca en la mayoría de las ocasiones
+    clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Intentamos la conexión del cliente
+    if (client.connect(clientId.c_str())) {
+      String printLine = "   Cliente " + clientId + " conectado al servidor" + mqtt_server;
+      Serial.println(printLine);
+      // Publicamos un mensaje en el canal indicando que el cliente se ha
+      // conectado. Esto avisaría al resto de clientes que hay un nuevo
+      // dispositivo conectado al canal. Puede ser interesante en algunos casos.
+      String body = "Dispositivo con ID = ";
+      body += clientId;
+      body += " conectado al canal ";
+      body += channel_name;
+      client.publish(channel_name, "");
+      // Y, por último, suscribimos el cliente al canal para que pueda
+      // recibir los mensajes publicados por otros dispositivos suscritos.
+      client.subscribe(channel_name);
+    } else {
+      Serial.print("Error al conectar al canal, rc=");
+      Serial.print(client.state());
+      Serial.println(". Intentando de nuevo en 5 segundos.");
+      delay(5000);
+    }
+  }
+}
+
+
+// Método para hacer una petición GET al servidor REST
+void makeGetRequest(){
+    HTTPClient http;
+    // Abrimos la conexión con el servidor REST y definimos la URL del recurso
+    String url = "http://";
+    url += http_server;
+    url += ":";
+    url += http_server_port;
+    url += "/comidaProxima/";
+    String message = "Enviando petición GET al servidor REST. ";
+    message += url;
+
+    Serial.println(message);
+
+    http.begin(url);
+    // Realizamos la petición y obtenemos el código de estado de la respuesta
+    int httpCode = http.GET();
+    Serial.printf("\nRespuesta servidor REST %d\n", httpCode);
+
+
+    if (httpCode > 0)
+    {
+     // Si el código devuelto es > 0, significa que tenemos respuesta, aunque
+     // no necesariamente va a ser positivo (podría ser un código 400).
+     // Obtenemos el cuerpo de la respuesta y lo imprimimos por el puerto serie
+     String payload = http.getString();
+     Serial.println("payload: " + payload);
+
+     const size_t bufferSize = JSON_OBJECT_SIZE(1) + 370;
+     DynamicJsonDocument root(bufferSize);
+     deserializeJson(root, payload);
+     const char* Nombre= root["nombre"];
+     hora = root[0]["horas"];
+     peso = root[0]["pesosPlato"];
+
+     Serial.println(root.as<String>());
+
+  }
+
+
+    // Cerramos la conexión con el servidor REST
+    http.end();
+}
+
+void dispensar( int peso){
+  fsrReading = analogRead (fsrPin);
+  //double pesoL= (fsrReading*10000.0)/1023.0;
+  while (fsrReading<peso){
+    fsrReading = analogRead (fsrPin);
+    //double pesoL= (fsrReading*10000.0)/1023.0;
+    Serial.printf("Peso leido: %d",fsrReading );
+    Serial.printf("Peso total: %d ", peso);
+    servo.write(90);
+    delay(100);
+    servo.write(0);
+    delay(1000);
+
+
+  }
+  delay(60000);
+  makeGetRequest();
+  Serial.printf("Peso nuevo: %d",  peso);
+
+
+
+}
+
+// Método para hacer una petición PUT al servidor REST
+void makePutRequest(){
+    HTTPClient http;
+    // Abrimos la conexión con el servidor REST y definimos la URL del recurso
+    String url = "http://";
+    url += http_server;
+    url += ":";
+    url += http_server_port;
+    url += "/comidaProxima";
+
+    String message = "Enviando petición PUT al servidor REST. ";
+    message += url;
+    Serial.println(message);
+    // Realizamos la petición y obtenemos el código de estado de la respuesta
+    http.begin(url);
+
+    const size_t bufferSize = JSON_OBJECT_SIZE(1) + 370;
+    DynamicJsonDocument root(bufferSize);
+    root["last_update"] = 12412321;
+    root["value"] = "asf213124";
+    String json_string;
+    serializeJson(root, json_string);
+
+    int httpCode = http.PUT(json_string);
+
+    if (httpCode > 0)
+    {
+     // Si el código devuelto es > 0, significa que tenemos respuesta, aunque
+     // no necesariamente va a ser positivo (podría ser un código 400).
+     // Obtenemos el cuerpo de la respuesta y lo imprimimos por el puerto serie
+     String payload = http.getString();
+     Serial.println("payload put: " + payload);
+    }
+
+    Serial.printf("\nRespuesta servidor REST PUT %d\n", httpCode);
+    // Cerramos la conexión con el servidor REST
+    http.end();
+}
+
+// Método de inicialización de la lógica
+void setup() {
+  // Ajustamos el pinmode del pin de salida para poder controlar un
+  // switch digial (dido led por ejemplo)
+  pinMode(BUILTIN_LED, OUTPUT);
+  // Fijamos el baudrate del puerto de comunicación serie
+
+  Serial.begin(115200); // activamos la conexión serial
+  servo.attach(2);//D4
+  servo.write(0);
+  // Nos conectamos a la red WiFi
+  setup_wifi();
+  // Indicamos la dirección y el puerto del servidor donde se encuentra el
+  // servidor MQTT
+  client.setServer(mqtt_server, 1883);
+  // Fijamos la función de callback que se ejecutará cada vez que se publique
+  // un mensaje por parte de otro dispositivo en un canal al que el cliente
+  // actual se encuentre suscrito
+  client.setCallback(callback);
+    makeGetRequest();
+}
+
+void loop() {
+
+
+  // Nos conectamos al servidor MQTT en caso de no estar conectado previamente
+  /*if (!client.connected()) {
+    reconnect();
+  }
+  // Esperamos (de manera figurada) a que algún cliente suscrito al canal
+  // publique un mensaje que será recibido por el dispositivo actual
+  client.loop();
+
+  // Cada 2 segundos publicaremos un mensaje en el canal procedente del cliente
+  // actual. Esto se hace sin bloquear el loop ya que de lo contrario afectaría
+  // a la recepción de los mensajes MQTT
+/*  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    // Construimos un objeto JSON con el contenido del mensaje a publicar
+    // en el canal.
+    StaticJsonDocument<200> doc;
+    doc["clientId"] = clientId;
+    doc["message"] = "periodic message";
+    doc["number"] = value;
+    String output;
+    serializeJson(doc, output);
+    Serial.print("Mensaje publicado: ");
+    Serial.println(output);
+    client.publish(channel_name, output.c_str());
+  }
+
+
+  if (now - lastMsgRest > 2000) {
+    lastMsgRest = now;
+    makeGetRequest();
+    makePutRequest();
+  }
+*/
+
+  timeClient.update();
+
+  Serial.println(timeClient.getFormattedTime());
+  if((timeClient.getHours())*100+(timeClient.getMinutes()) == hora){
+    dispensar(peso);
+  }else{
+
+    Serial.printf("%d\n", peso);
+    Serial.printf("%d\n", hora);
+    Serial.println("Todavia no");
+  }
+
+  delay(6000);
+
+}
